32f500d15895b218a58165fd5f247db315e57c6c
lokideos <archmage09@gmail.com>
2018-09-24 20:32:25 +0300

    Add seats to passenger cars

--- app_data/passenger_car.rb
+++ app_data/passenger_car.rb
@@ -1,5 +1,19 @@
 class PassengerCar < Car
-  def initialize(number, manufacturer)
-    super
+  attr_accessor :seats
+  attr_reader :max_seats
+
+  def initialize(number, manufacturer, seats)
+    super(number, manufacturer)
+
+    @max_seats = seats
+    @seats = 0
+  end
+
+  def take_seat
+    self.seats += 1 if seats < max_seats
+  end
+
+  def free_seats
+    max_seats - seats
   end
 end
