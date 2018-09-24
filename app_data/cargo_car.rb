4427d5806f32c0b8407010be278e97f7ac5408bb
lokideos <archmage09@gmail.com>
2018-09-24 20:38:33 +0300

    Add volume to cargo cars

--- app_data/cargo_car.rb
+++ app_data/cargo_car.rb
@@ -1,5 +1,19 @@
 class CargoCar < Car
-  def initialize(number, manufacturer)
-    super
+  attr_accessor :volume
+  attr_reader :max_volume
+
+  def initialize(number, manufacturer, volume)
+    super(number, manufacturer)
+
+    @max_volume = volume
+    @volume = 0
+  end
+
+  def load(volume)
+    self.volume += volume if self.volume + volume <= max_volume
+  end
+
+  def free_volume
+    max_volume - volume
   end
 end
