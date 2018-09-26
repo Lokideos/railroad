782ff36d93a69b5fe96c25e0b3111b02bc1e9278
lokideos <archmage09@gmail.com>
2018-09-27 00:55:54 +0300

    Format validation

--- app_data/support/validation.rb
+++ app_data/support/validation.rb
@@ -7,13 +7,20 @@ module Validation
   end
 
   module ClassMethods
-    def validate(attr_name, validation_type, *_args)
+    def validate(attr_name, validation_type, *args)
       attribute_name = attr_name.to_sym
-
       validation_type_name = validation_type.to_s.to_sym
+      option = args[0]
+
       if validation_type_name == :presence
         define_method(validation_type_name) do
           raise "Object @#{attribute_name} doesn't exist" if instance_variable_get("@#{attribute_name}".to_sym).nil?
+        end
+      end
+
+      if validation_type_name == :format
+        define_method(validation_type_name) do
+          raise "Wrong format of @#{attribute_name}" if instance_variable_get("@#{attribute_name}".to_sym) !~ option
         end
       end
     end
@@ -22,6 +29,7 @@ module Validation
   module InstanceMethods
     def validate!
       presence if methods.find { |method_name| method_name == :presence }
+      format if methods.find { |method_name| method_name == :format }
       true
     end
 
